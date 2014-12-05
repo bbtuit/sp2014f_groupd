7f45ac8b4b6453252fd0bc02bbcafaf0b18f9ab4
nakamurayoshinobu <nakamurayoshinobu@192.168.0.4>
2014-12-05 21:03:44 +0900

    画面4について画面遷移まで追加しました。中村

---
+++ src/ac/bbt/sp2014f_groupd/FourthlayoutActivity.java
@@ -0,0 +1,69 @@
+package ac.bbt.sp2014f_groupd;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class FourthlayoutActivity extends Activity { // Activityクラスを継承
+
+	// onCreateメソッド(画面初期表示イベントハンドラ)
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+		// スーパークラスのonCreateメソッド呼び出し
+        super.onCreate(savedInstanceState);
+        // レイアウト設定ファイルの指定
+        setContentView(R.layout.fragment_fourthlayout);
+
+    	// インテント取得
+    	Intent data = getIntent();
+        
+		// ボタンオブジェクト取得
+		Button button1 = (Button)findViewById(R.id.button4_1);
+		// ボタンオブジェクトにクリックリスナー設定
+		button1.setOnClickListener(new ButtonClickListener1());
+		
+		// ボタンオブジェクト取得
+		Button button2 = (Button)findViewById(R.id.button4_2);
+		// ボタンオブジェクトにクリックリスナー設定
+		button2.setOnClickListener(new ButtonClickListener2());
+		
+	}
+
+	// クリックリスナー定義
+	class ButtonClickListener1 implements OnClickListener {
+		// onClickメソッド(ボタンクリック時イベントハンドラ)
+		public void onClick(View v) {
+
+		//まだ作成していません。中村
+			
+		}
+	}
+	
+	// クリックリスナー定義
+	class ButtonClickListener2 implements OnClickListener {
+		// onClickメソッド(ボタンクリック時イベントハンドラ)
+		public void onClick(View v) {
+			// アクティビティ終了(画面クローズ)
+			finish();
+		}
+	}
+
+	// onCreateOptionsMenuメソッド(オプションメニュー生成)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // オプションメニューを設定
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
+
--- src/ac/bbt/sp2014f_groupd/MainActivity.java
+++ src/ac/bbt/sp2014f_groupd/MainActivity.java
@@ -3,16 +3,89 @@ package ac.bbt.sp2014f_groupd;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.os.Build;
 
+public class MainActivity extends Activity {
+
+	// onCreateメソッド(画面初期表示イベントハンドラ)
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+		// スーパークラスのonCreateメソッド呼び出し
+        super.onCreate(savedInstanceState);
+        // レイアウト設定ファイルの指定
+        setContentView(R.layout.fragment_main);
+
+		// ボタンオブジェクト取得
+		Button button1 = (Button)findViewById(R.id.button1_1);
+		// ボタンオブジェクトにクリックリスナー設定
+		button1.setOnClickListener(new ButtonClickListener1());
+
+		// ボタンオブジェクト取得
+		Button button2 = (Button)findViewById(R.id.button1_2);
+		// ボタンオブジェクトにクリックリスナー設定
+		button2.setOnClickListener(new ButtonClickListener2());
+
+		// ボタンオブジェクト取得
+		Button button3 = (Button)findViewById(R.id.button1_3);
+		// ボタンオブジェクトにクリックリスナー設定
+		button3.setOnClickListener(new ButtonClickListener3());
+		
+	}
+
+	// クリックリスナー定義
+	//画面2への遷移情報
+	class ButtonClickListener1 implements OnClickListener {
+		// onClickメソッド(ボタンクリック時イベントハンドラ)
+		public void onClick(View v) {
+
+			// 画面2の情報を追記
+
+		}
+	}
+	
+	//画面3への遷移情報
+	class ButtonClickListener2 implements OnClickListener {
+		// onClickメソッド(ボタンクリック時イベントハンドラ)
+		public void onClick(View v) {
+
+			// 画面3の情報を追記
+
+		}
+	}
+
+	//画面4への遷移情報
+	class ButtonClickListener3 implements OnClickListener {
+		// onClickメソッド(ボタンクリック時イベントハンドラ)
+		public void onClick(View v) {
+
+			// インテントの生成(呼び出すクラスの指定)
+			Intent intent = new Intent(MainActivity.this, FourthlayoutActivity.class);
+			// 次のアクティビティの起動
+			startActivity(intent);
+		}
+	}
+	
+	// onCreateOptionsMenuメソッド(オプションメニュー生成)
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+		// オプションメニューを設定
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
 
 
+//以下は除外しました。何か問題があれば連絡をお願い致します。中村
+/*
 public class MainActivity extends Activity {
 
     @Override
@@ -46,9 +119,10 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+
     /**
      * A placeholder fragment containing a simple view.
-     */
+     
     public static class PlaceholderFragment extends Fragment {
 
         public PlaceholderFragment() {
@@ -62,3 +136,4 @@ public class MainActivity extends Activity {
         }
     }
 }
+*/
